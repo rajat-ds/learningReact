77dba88dd874cf62bb9f3964bc46b6da41d84eef
rajat-ds <rajatkumarbind@gmail.com>
2021-06-14 10:07:00 +0530

    implemented useSelector Hook

--- react-Redux/learning-redux/src/App.js
+++ react-Redux/learning-redux/src/App.js
@@ -1,12 +1,14 @@
 import { Provider } from "react-redux";
 import "./App.css";
 import CakeContainer from "./components/CakeContainer";
+import HooksCakeContainer from "./components/HooksCakeContainer";
 import store from "./redux/store";
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <CakeContainer />
+        <HooksCakeContainer/>
       </div>
     </Provider>
   );
