af7219c5e3aa337e55c944d916dd29607da9fbe1
rajat-ds <rajatkumarbind@gmail.com>
2021-06-16 10:43:04 +0530

    second argument in mapStateToProps

--- react-Redux/learning-redux/src/App.js
+++ react-Redux/learning-redux/src/App.js
@@ -4,11 +4,14 @@ import NewCakeContainer from "./components/NewCakeContainer";
 import CakeContainer from "./components/CakeContainer";
 import HooksCakeContainer from "./components/HooksCakeContainer";
 import IceCreamContainer from "./components/IceCreamContainer";
+import ItemContainer from "./components/ItemContainer";
 import store from "./redux/store";
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer/>
         <CakeContainer />
         <HooksCakeContainer/>
         <IceCreamContainer/>
