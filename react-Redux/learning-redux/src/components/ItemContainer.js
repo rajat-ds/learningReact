c3881d98ba900996ceaa8702d7eb693258b73e39
rajat-ds <rajatkumarbind@gmail.com>
2021-06-16 11:44:36 +0530

    second argumnet in mapDispatchToProps

--- react-Redux/learning-redux/src/components/ItemContainer.js
+++ react-Redux/learning-redux/src/components/ItemContainer.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
+import { buyCake, buyIceCream } from "../redux";
 
 const ItemContainer = (props) => {
-    return (
-        <div>
-            <h2> Item - {props.item} </h2>
-            
-        </div>
-    );
+  return (
+    <div>
+      <h2> Item - {props.item} </h2>
+      <button onClick={props.buyItem}> Buy Items</button>
+    </div>
+  );
 };
 
-const mapStateToProps =(state,ownProps)=>{
-   const itemState = ownProps.cake ? state.cake.numOfCakes : state.iceCream.numOfIceCream
-   return  {
-      item:itemState
-   }
+const mapStateToProps = (state, ownProps) => {
+  const itemState = ownProps.cake
+    ? state.cake.numOfCakes
+    : state.iceCream.numOfIceCream;
+  return {
+    item: itemState,
+  };
+};
 
+const mapDispatchToProps = (disPatch, ownProps) => {
+  const disPatchFunction = ownProps.cake
+    ? () => disPatch(buyCake())
+    : () => disPatch(buyIceCream());
 
-}
+  return {
+    buyItem: disPatchFunction,
+  };
+};
 
-
-export default  connect(mapStateToProps)(ItemContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(ItemContainer);
