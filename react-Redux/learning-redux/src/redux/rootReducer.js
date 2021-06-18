d92fa52bde677c5b6f70db427d265608b289273f
rajat-ds <rajatkumarbind@gmail.com>
2021-06-18 11:03:04 +0530

    async action

--- react-Redux/learning-redux/src/redux/rootReducer.js
+++ react-Redux/learning-redux/src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import  iceCreamReducer  from "./iceCream/iceCreamReducer";
 import  cakeReducer  from "./cake/cakeReducer";
+import userReducer from "./user/userReducer";
 
 const rootReducer = combineReducers({
   cake: cakeReducer,
   iceCream: iceCreamReducer,
+  user:userReducer
 });
 
 export default rootReducer;
